343ab6447c3838e97df7b20f54915c8d14fb8d17
Anathalia Izidro <49588396+AnathaliaIzidro@users.noreply.github.com>
2024-07-12 12:08:54 -0300

    Add files via upload

---
+++ converter.py
@@ -0,0 +1,11 @@
+# 4. Crie uma variável e nela armazene um valor em reais em seguida crie outra variável para armazenar este valor convertido em dolar, considere a cotação do dolar 5.44
+
+# real = 1000
+# dolar = real / 5.44
+# print("Valor em dolar:", dolar)
+
+valor = 489
+cota = 5.44
+
+dolar = valor / cota
+print(f'O valor em dolar é: {dolar:.2f}')
